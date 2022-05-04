f03e02cf102fdd7814854eaa6aa6f7b511b92628
gautamsharma3107 <55848552+gautamsharma3107@users.noreply.github.com>
2022-05-04 09:33:43 +0530

    Create PhoneNumberValidation.js

---
+++ PhoneNumberValidation.js
@@ -0,0 +1,22 @@
+"use strict";
+//exports.__esModule = true;
+function checkPhoneNumber(phoneNumber) {
+    if (phoneNumber.length < 1) {
+        return -1;
+    }
+    else {
+        if (phoneNumber.match(/^[0-9-]+$/) != null) {
+            if ((phoneNumber.replace(/[-]/g, '')).length == 10) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+}
+var ph_number = prompt("Enter Phone Number");
+console.log(checkPhoneNumber(ph_number));
